944dd857821aa5920b9afbf87adbd5538b5fc918
Naeem_Upgrade_PC <naeemgg@duck.com>
2022-03-28 19:48:56 +0530

    Displayed product as well as created customer.csv

--- invoicing_project/DispProd.php
+++ invoicing_project/DispProd.php
@@ -12,24 +12,71 @@ include "same_header.php";
 <body>
     <h1>DISPLAY PRODUCTS</h1>
     <form method="post">
+        <select name='slt'>
+            <option>Select customer name</option>
+            <?php
+            $f=fopen("customers.csv","r");
+            while(($row=fgetcsv($f))!==false)
+            {
+                echo "<option value=$row[0]>";
+                echo $row[0];
+                echo "</option>";
+            }
+            fclose($f);
+            ?>
+        </select> <br><br>
         <input type="submit" value="Retrive All" name="sub">
-    </form>
-    <?php
+        <?php
     if(isset($_POST['sub']))
     {
-        $f=fopen("products.csv","r");
-        echo "<table border=1 cellspacing=0 cellpadding=5>";
-        while(($row=fgetcsv($f))!==false)
-        {
-           echo "<tr>";
-           foreach ($row as $r) 
-           {
-             echo "<td>".$r."</td>";
-           }
-           echo "</tr>";
+        $f1=fopen("customers.csv","r");
+            $r=0;
+            $selected_name=$_POST['slt'];
+            echo "<table border=1>";
+            while(($row1=fgetcsv($f1))!==false)
+            {
+                echo "<tr>";
+                if($row1[0]===$selected_name)
+                {
+                    foreach ($row1 as $r1) {
+                        echo "<td>$r1</td>";
+                    }
+                }
+                else
+                {
+                    echo "not matched";
+
+                }
+                echo "</tr>";
+                $r++;
+            }
+            echo "</table>";
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+            
+            // $f=fopen("products.csv","r");
+            // echo "<table border=1 cellspacing=0 cellpadding=5>";
+            // while(($row=fgetcsv($f))!==false)
+        // {
+            //    echo "<tr>";
+        //    foreach ($row as $r) 
+        //    {
+            //      echo "<td>".$r."</td>";
+            //    }
+            //    echo "</tr>";
+            // }
+            // echo "</table>";
         }
-        echo "</table>";
-    }
-    ?>
+        ?>
+</form>
 </body>
 </html>
--- invoicing_project/SalesInquery.php
+++ invoicing_project/SalesInquery.php
@@ -18,12 +18,58 @@ include "same_header.php";
 <body>
     <h1>SALES INQUERY</h1>
     <form method="post">
-        <input type="text" name="cname" placeholder="Enter customer name">
-        <input type="number" name="cphone" placeholder="Enter phone number">
+        <input type="text" name="cname" placeholder="Enter customer name" required>
+        <input type="number" name="cphone" placeholder="Enter phone number" required><br><br>
         <?php
-        
+            $price=0;
+            $qty=1000;
+            $f=fopen("products.csv","r");
+            echo "<table border=1 cellspacing=0 cellpadding=5 style='text-align:center;'>";
+            echo "<tr><th>checkbox</th><th>Product Name</th><th>Product Price</th><th>Product Quantity</th></tr>";
+            while(($row=fgetcsv($f))!==false)
+            {
+                echo "<tr>";
+                echo "<td><input type='checkbox' value='$row[0]' name='$row[1]' checked>"."</td>";
+                echo "<td>$row[0]</td>";
+                echo "<td><input type='text' name=$price value=$row[1] readonly size='5'></td>";
+                echo "<td><input type='number' name=$qty value=0 size=5></td>";
+                echo "</tr>";
+                $price++;
+                $qty++;
+            }
+            echo "</table>";
+            fclose($f); 
         ?>
-        <input type="submit" value="Save Records" name="sub">
+        <br>
+        <input type="submit" value="Save Records" name="sub"><br>
+        <?php
+
+        if(isset($_POST['sub']))
+        {
+            
+            //print_r($_POST);
+            //echo "<br>";
+            $values=array_values($_POST);
+            // echo "values: <br>";
+            // print_r($values);
+            // foreach ($values as $key) 
+            // {
+            //     echo "<br>$key";
+            // }
+            $f1=fopen("customers.csv","a");
+            $pop=array_pop($values);
+                // print_r($pop);
+            if(($check=fputcsv($f1,$values))!==false)
+            {
+                echo "<script>alert('Data Saved Successfully');</script>";
+            }
+            else
+            {
+                die("ERROR! While writing customer.csv");
+            }
+            fclose($f1);
+        }
+        ?>
     </form>
 </body>
 </html>
