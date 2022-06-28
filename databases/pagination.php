fc10b203ea1f4c3b0bdc80005f906a3801546613
Naeem_Upgrade_PC <naeemgg@duck.com>
2022-06-28 19:23:29 +0530

    added prev and next

--- databases/pagination.php
+++ databases/pagination.php
@@ -7,6 +7,26 @@
             width:250px;
             /* background-color:red; */
         }
+        a
+        {
+            background-color:grey;
+            border-radius:10px;
+            margin-left:60px;
+            /* margin:0px auto; */
+            padding:15px;
+            text-decoration:none;
+            color:white;
+            font-size:18px;
+
+        }
+        div 
+        {
+            /* background-color:red; */
+            margin:0px auto;
+            width:50%;
+            height:100px;
+        }
+        
         </style>
     <title> Pagination </title>  
     </head>  
@@ -49,52 +69,69 @@
 
 
         }
-        //     $result = $conn->query($query);
+            $result = $conn->query($query);
 
-        //     // $row = $result->fetch_assoc();
-        //     // print_r($row);
-        //     echo "<table align=center border=1 cellspacing=0 cellpadding=20 style='width:60%; margin-top:100px;'>";
-        //     echo "<tr><th>Sr No</th><th>Language Name</th><th>Year</th><th>Image</th><th class='d'>Description</th><th>Time</th><th>Delete</th><th>Update</th></tr>";
-        //     while ($row = $result->fetch_assoc()) 
-        //     {  
-        //         echo "<tr>";
-        //         echo "<td>";
-        //         $s=$_SESSION['srno']=$row['srno'];
-        //         echo $row["srno"];
-        //         echo "</td>";
+            // $row = $result->fetch_assoc();
+            // print_r($row);
+            echo "<table align=center border=1 cellspacing=0 cellpadding=20 style='width:60%; margin-top:100px;'>";
+            echo "<tr><th>Sr No</th><th>Language Name</th><th>Year</th><th>Image</th><th class='d'>Description</th><th>Time</th><th>Delete</th><th>Update</th></tr>";
+            while ($row = $result->fetch_assoc()) 
+            {  
+                echo "<tr>";
+                echo "<td>";
+                $s=$_SESSION['srno']=$row['srno'];
+                echo $row["srno"];
+                echo "</td>";
                 
-        //         echo "<td>";
-        //         echo $row["lang_name"];
-        //         echo "</td>";
+                echo "<td>";
+                echo $row["lang_name"];
+                echo "</td>";
                 
-        //         echo "<td>";
-        //         echo $row["year"];
-        //         echo "</td>";
+                echo "<td>";
+                echo $row["year"];
+                echo "</td>";
                 
-        //         echo "<td>";
-        //         echo "<img src={$row["lang_logo"]} width=100>";
-        //         echo "</td>";
+                echo "<td>";
+                echo "<img src={$row["lang_logo"]} width=100>";
+                echo "</td>";
 
-        //         echo "<td class='d'>";
-        //         echo $row["lang_desc"];
-        //         echo "</td>";
+                echo "<td class='d'>";
+                echo $row["lang_desc"];
+                echo "</td>";
                 
-        //         echo "<td>";
-        //         echo $row["date"];
-        //         echo "</td>";
-        //         echo  "<td><button onclick='del($s);'>Delete</button></td>";
+                echo "<td>";
+                echo $row["date"];
+                echo "</td>";
+                echo  "<td><button onclick='del($s);'>Delete</button></td>";
 
-        //         echo  "<td><button onclick='upd();'>Update</button></td>";
+                echo  "<td><button onclick='upd();'>Update</button></td>";
 
                 
-        //         echo "</tr>";
-        //     }  
-        //     echo "</table>";
+                echo "</tr>";
+            }  
+            echo "</table>
+            
+            <br><br><div>";
+            if($page>1)
+            {
+                $prev=$page-1;
+                echo "<a href=?page=$prev >Prev</a>";
+            }
+            
+            for($page = 1; $page<= $num_pages; $page++) {  
+                echo '<a href = "pagination.php?page=' . $page . '">' . $page . ' </a>';  
+            
+            }
+            
+            if($_GET['page']<$num_pages)
+            {   
+                echo "<h1>{$_GET['page']}</h1>";
 
-        //     for($page = 1; $page<= $num_pages; $page++) {  
-        //         echo '<a href = "pagination.php?page=' . $page . '">' . $page . ' </a>';  
-        //     }  
-        // }
+                // echo "<h1>$total_results<br>$num_pages</h1>";
+                $next=$_GET['page']+1;
+                echo "<a href=?page=$next >Next</a>"; 
+            }  
+            echo "</div>";
           
       /*
         //define total number of results you want per page  
